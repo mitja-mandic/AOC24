d22d6aac242864290e812ee6e1f188c8ed45b3d1
mitja-mandic <mitja.mandic@gmail.com>
2024-12-01 11:57:32 +0100

    dopamin to je to

--- day 1/day1.py
+++ day 1/day1.py
@@ -1,9 +1,21 @@
 list1 = []
 list2 = []
-with open("day1_input.txt",'r') as f:   
+fileLoc = r'C:/Users/Mitja/Documents/AOC24/AOC24/day 1/day1_input.txt'
+with open(fileLoc,'r') as f:   
     for line in f:
         values = line.split()
         list1.append(values[0])
         list2.append(values[1])
 
-    
+#tuplesList = zip(sorted(list1), sorted(list2))
+#
+#result = 0
+#for x in tuplesList:
+#    result += abs(int(x[0])-int(x[1]))
+
+#print(result)
+result2 = 0
+for x in list1:
+    repetitions = list2.count(x)
+    result2 += int(x) * repetitions
+print(result2)
